13b957a71f3b8a15b1efb21992db934a6722463e
Andrew Shal <shalandrew97@gmail.com>
2020-07-08 03:42:33 -0600

    converted app widget to statful widget, add a counter on add image button click, the counter is the state that is managed.

--- 5/pics/lib/main.dart
+++ 5/pics/lib/main.dart
@@ -1,29 +1,11 @@
 // need to import helper library
-// from flutter to get contetn on the screen
+// from flutter to get content on the screen
+// Import our custom widget
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 // Define a 'main' function to run when our app starts
 void main() {
-  // Create a new text widget to show some text on the screen
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My Flutter App'),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // add on pressed code here
-          print('floating button pressed!');
-        },
-        label: Text('Add Image'),
-        icon: Icon(
-          Icons.add_a_photo,
-        ),
-        backgroundColor: Colors.lime,
-      ),
-    ),
-  );
   // Take that widget and get it on the screen
-
-  runApp(app);
+  runApp(App());
 }
--- 5/pics/lib/src/app.dart
+++ 5/pics/lib/src/app.dart
@@ -2,7 +2,47 @@
 import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget
-// This class must extend the 'StatelessWidget' base class
+// This class must extend the 'StatefulWidget' base class
+// this has a _counter state
+class App extends StatefulWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+// the logic for the stateful widget
+class _AppState extends State<App> {
+  // instance var that will change over time
+  int _counter = 0;
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Flutter App'),
+        ),
+        body: Center(
+          child: Text('$_counter images'),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            // add on pressed code here
+            setState(() {
+              _counter += 1;
+            });
+          },
+          label: Text('Add Image'),
+          icon: Icon(
+            Icons.add_a_photo,
+          ),
+          backgroundColor: Colors.lime,
+        ),
+      ),
+    );
+  }
+}
 
 // Must define a 'build' method that returns
 // the widget that *this* widget will show
